232c55af9f7012b01eb9a836d3faf6d9500136db
홍규진 <hkj0206@dgu.ac.kr>
2025-03-29 15:43:10 +0900

    feat: vite plugin 의존성 패키지 추가 (svg)

--- cama/vite.config.ts
+++ cama/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import svgr from "vite-plugin-svgr";
 
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
+  plugins: [react(), vanillaExtractPlugin(), svgr()],
 });
