518607df5a77c948fb80b14ea79a68f80498cf8d
홍규진 <hkj0206@dgu.ac.kr>
2025-04-05 14:34:56 +0900

    feat: Contexts 적용

--- cama/src/main.tsx
+++ cama/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthProvider, ModalProvider } from "./contexts";
+import { CookiesProvider } from "react-cookie";
+import { Toaster } from "sonner";
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ModalProvider>
+      <CookiesProvider>
+        <AuthProvider>
+          <App />
+          <Toaster />
+        </AuthProvider>
+      </CookiesProvider>
+    </ModalProvider>
+  </StrictMode>
+);
