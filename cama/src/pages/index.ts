41e74ba4e55b42067986f975c3841a81ef955657
홍규진 <hkj0206@dgu.ac.kr>
2025-04-05 14:34:26 +0900

    feat: pages exports 정리

---
+++ cama/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as MainPage } from "./main";
+export { default as ProductsPage } from "./products";
+export { default as AdminLoginPage } from "./auth";
+// export { default as ProductDetailPage } from "./product-detail";
