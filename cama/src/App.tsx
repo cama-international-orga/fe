274275e2da42a69da6255320bf5a3a3cbef83c66
홍규진 <hkj0206@dgu.ac.kr>
2025-04-05 14:35:03 +0900

    feat: 라우팅 추가

--- cama/src/App.tsx
+++ cama/src/App.tsx
@@ -1,8 +1,24 @@
 import "./App.css";
-import MainPage from "./pages/main";
+import Header from "./components/Header";
+import { MainPage, ProductsPage, AdminLoginPage } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useAuth } from "./contexts";
 
 function App() {
-  return <MainPage/>;
+  const { isLoggedIn } = useAuth();
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route
+          path="/products"
+          element={<ProductsPage isLoggedIn={isLoggedIn} />}
+        />
+        <Route path="/admin" element={<AdminLoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
