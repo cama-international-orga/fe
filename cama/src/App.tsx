316120d661895610fe466b81754739afa0128f41
홍규진 <hkj0206@dgu.ac.kr>
2025-04-28 13:34:55 +0900

    fix: categoryPath로 명명법 수정

--- cama/src/App.tsx
+++ cama/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route
-          path="/categories/:categoryId"
+          path="/categories/:categoryPath"
           element={<CategoryDetailPage isLoggedIn={isLoggedIn} />}
         />
         <Route path="/admin" element={<AdminLoginPage />} />
