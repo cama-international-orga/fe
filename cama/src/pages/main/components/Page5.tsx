ef3136254b6c85d041e43231fb0ff23108b26492
홍규진 <hkj0206@dgu.ac.kr>
2025-05-03 17:38:08 +0900

    feat: MainPage TrueFocus 컴포넌트 구현

--- cama/src/pages/main/components/Page5.tsx
+++ cama/src/pages/main/components/Page5.tsx
@@ -1,15 +1,15 @@
 // components/Page3.tsx
 import { motion } from "framer-motion";
 import Footer from "../../../components/Footer";
+// import TrueFocus from "./TrueFocus";
 
 const Page5 = () => {
   return (
     <motion.div
       transition={{ type: "spring" }}
-      style={{ width: "100%", height: "100%", margin: "0 auto" }}
+      style={{ width: "100%", margin: "0 auto", overflow: "hidden" }}
     >
-      <h1>5th Page</h1>
-      <p>3D Rotation Effect</p>
+      {/* <TrueFocus sentence="Focus on the moment" /> */}
       <Footer />
     </motion.div>
   );
